8e7d12dd6b9837050219dfa493260bfc88f894a1
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-08-15 14:44:46 +0530

    Create diff between the  number.java

---
+++ diff between the  number.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=10;
+		int m=14;
+		System.out.print(m-n);
+	}
+}
